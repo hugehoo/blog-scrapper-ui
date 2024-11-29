93c82051891178a50ff131e0b5f3ed3c07c62986
hoo <tbnsok40@gmail.com>
2024-11-29 17:49:52 +0900

    fix: constants api

--- src/types/ApiConstants.ts
+++ src/types/ApiConstants.ts
@@ -1,2 +1,2 @@
-// export const API_HOST = "https://j7sj1zu2ve.execute-api.ap-northeast-2.amazonaws.com/prod";
-export const API_HOST = "http://localhost:8080";
+export const API_HOST = "https://j7sj1zu2ve.execute-api.ap-northeast-2.amazonaws.com/prod";
+// export const API_HOST = "http://localhost:8080";

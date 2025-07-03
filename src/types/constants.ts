a5f3c4ba5815da0c6ac97a9bbba20eb41f6e452b
hoo <tbnsok40@gmail.com>
2025-07-03 16:07:57 +0900

    feat: add woowa constants

--- src/types/constants.ts
+++ src/types/constants.ts
@@ -1,117 +1,121 @@
 export const Corps = {
   KAKAOPAY: {
     name: "KAKAOPAY",
-    color: "#FFD600"
+    color: "#FFD600",
   },
   NAVERPAY: {
     name: "NAVERPAY",
-    color: "#0FDE5A"
+    color: "#0FDE5A",
   },
   KAKAOBANK: {
     name: "KAKAOBANK",
-    color: "#FFD600"
+    color: "#FFD600",
   },
   TOSS: {
     name: "TOSS",
-    color: "#2B77F5"
+    color: "#2B77F5",
   },
   DAANGN: {
     name: "DAANGN",
-    color: "#ff6411"
+    color: "#ff6411",
   },
   OLIVE: {
     name: "OLIVE",
-    color: "#9ACC2D"
+    color: "#9ACC2D",
   },
   BUZZVIL: {
     name: "BUZZVIL",
-    color: "#F23B2F"
+    color: "#F23B2F",
   },
   MUSINSA: {
     name: "MUSINSA",
-    color: "#000000"
+    color: "#000000",
   },
   TWONINE: {
     name: "29CM",
-    color: "#000000"
+    color: "#000000",
   },
   KURLY: {
     name: "KURLY",
-    color: "#530074"
+    color: "#530074",
   },
   DEVSISTERS: {
     name: "DEVSISTERS",
-    color: "#FF5400"
+    color: "#FF5400",
   },
   OHOUSE: {
     name: "OHOUSE",
-    color: "#2FBCEB"
+    color: "#2FBCEB",
   },
   LINE: {
     name: "LINE",
-    color: "#0CAA34"
+    color: "#0CAA34",
   },
   SOCAR: {
     name: "SOCAR",
-    color: "#00AEF2"
-  }
+    color: "#00AEF2",
+  },
 } as const;
 
 export const CorpList = [
   {
     name: "KAKAOPAY",
-    color: "#FFD600"
+    color: "#FFD600",
   },
   {
     name: "NAVERPAY",
-    color: "#0FDE5A"
+    color: "#0FDE5A",
   },
   {
     name: "KAKAOBANK",
-    color: "#FFD600"
+    color: "#FFD600",
   },
   {
     name: "TOSS",
-    color: "#2B77F5"
+    color: "#2B77F5",
   },
   {
     name: "DAANGN",
-    color: "#ff6411"
+    color: "#ff6411",
   },
   {
     name: "OLIVE",
-    color: "#9ACC2D"
+    color: "#9ACC2D",
   },
   {
     name: "BUZZVIL",
-    color: "#F23B2F"
+    color: "#F23B2F",
   },
   {
     name: "MUSINSA",
-    color: "#000000"
+    color: "#000000",
   },
   {
     name: "29CM",
-    color: "#000000"
+    color: "#000000",
   },
   {
     name: "KURLY",
-    color: "#530074"
+    color: "#530074",
   },
   {
     name: "DEVSISTERS",
-    color: "#FF5400"
+    color: "#FF5400",
   },
   {
     name: "OHOUSE",
-    color: "#2FBCEB"
+    color: "#2FBCEB",
   },
   {
     name: "LINE",
-    color: "#0CAA34"
+    color: "#0CAA34",
   },
   {
     name: "SOCAR",
-    color: "#00AEF2"
-  }
+    color: "#00AEF2",
+  },
+  {
+    name: "WOOWA",
+    color: "#21C1BC",
+  },
 ] as const;
